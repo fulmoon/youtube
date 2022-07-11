b5a35e5dc698a90cef00ac84fe52fdd8801e7616
fulmoon <63829420+fulmoon@users.noreply.github.com>
2022-07-11 13:54:17 +0900

    STYLE Adapt ListView.separated on Mini

--- lib/youtube.dart
+++ lib/youtube.dart
@@ -3,17 +3,19 @@ import 'package:flutter/material.dart';
 class Youtube extends StatelessWidget {
   Youtube({Key? key}) : super(key: key);
 
-  final items =  ['홍길동', '한석봉', '이순신'];
+  final items = ['홍길동', '한석봉', '이순신'];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView.builder(
-        padding: const EdgeInsets.all(16),
-        itemCount: items.length,
-        itemBuilder: (BuildContext context, int index) { //함수, 새로 정의해야 함. index: 아이템의 인텍스 번호)
-          return Text('${items[index]}', style: TextStyle(fontSize: 100),);
-        }
+      body: ListView.separated(
+          padding: const EdgeInsets.all(16),
+          itemCount: items.length,
+          itemBuilder: (BuildContext context, int index) {
+            //함수, 새로 정의해야 함. index: 아이템의 인텍스 번호)
+            return Text('${items[index]}', style: TextStyle(fontSize: 100));
+          },
+          separatorBuilder: (BuildContext context, int index) => const Divider(),
       ),
     );
   }
